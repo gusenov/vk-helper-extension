f705c72ddad75f1ec4215236a6a4c5f5e1c7ab14
Аббас Гусенов <gusenov@live.ru>
2018-06-18 23:50:11 +0600

    Заготовка страницы для отображения списка постов со стены.

---
+++ list-of-all-posts-on-the-wall.js
@@ -0,0 +1,19 @@
+/*jslint
+    devel: true
+*/
+
+/*eslint-env
+    browser
+*/
+
+/* eslint-disable
+    no-console
+*/
+
+(function () {
+    'use strict';
+    
+    window.addEventListener("DOMContentLoaded", function () {
+    }, false);
+
+}());
